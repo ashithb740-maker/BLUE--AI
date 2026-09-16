fabbb1edeb8d96e9c749d0b90b1d6574d553a84d
Ashith B <ashithb740@gmail.com>
2026-09-16 20:00:35 +0530

    Route BLUE chat to Gemini-powered interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,10 @@ import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
-import Chat from "./pages/Chat";
+import BlueChat from "./pages/BlueChat";
 
 function Router() {
-  return <Switch><Route path="/" component={Home} /><Route path="/chat" component={Chat} /><Route path="/404" component={NotFound} /><Route component={NotFound} /></Switch>;
+  return <Switch><Route path="/" component={Home} /><Route path="/chat" component={BlueChat} /><Route path="/404" component={NotFound} /><Route component={NotFound} /></Switch>;
 }
 
 export default function App() {
