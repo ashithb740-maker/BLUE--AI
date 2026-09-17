a826dc6fb6bc14cd6c0b6a7980fba903b46c6107
Ashith B <ashithb740@gmail.com>
2026-09-17 21:37:40 +0530

    Make theme switching available across BLUE pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Switch, useLocation } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
+import ThemeToggle from "./components/ThemeToggle";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import BlueChat from "./pages/BlueChat";
@@ -12,6 +13,12 @@ function Router() {
   return <Switch><Route path="/" component={Home} /><Route path="/auth" component={Auth} /><Route path="/chat" component={BlueChat} /><Route path="/404" component={NotFound} /><Route component={NotFound} /></Switch>;
 }
 
+function GlobalThemeToggle() {
+  const [location] = useLocation();
+  if (location === "/") return null;
+  return <ThemeToggle />;
+}
+
 export default function App() {
-  return <ErrorBoundary><ThemeProvider defaultTheme="dark" switchable><TooltipProvider><Toaster theme="dark" /><Router /></TooltipProvider></ThemeProvider></ErrorBoundary>;
+  return <ErrorBoundary><ThemeProvider defaultTheme="dark" switchable><TooltipProvider><Toaster theme="dark" /><Router /><GlobalThemeToggle /></TooltipProvider></ThemeProvider></ErrorBoundary>;
 }
